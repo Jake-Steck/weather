637895c7d43fa59959053d30de1549d5912dba38
Jake Steck <jakestecklownj@gmail.com>
2022-05-22 11:00:49 -0400

    Python File containing the information to get weather with lat and lon

---
+++ main.py
@@ -0,0 +1,23 @@
+import geocoder # geocoder is a module that lets you get the lat and lon of a location
+import requests # requests is a module that lets you make a request to a url
+import math as m # math is a module that lets you do math, in this case it is used for its trunc() function
+
+
+location = input('\nEnter your desired location: ') 
+g = geocoder.location(location) # holds the coordinates of the location entered
+apiKey = '4a65195e5d4fd3acd1d3f92a9b362b8a' # api key for openweathermap.org
+lat = g.lat # holds the lat of the location you entered
+lon = g.lng # holds the lon of the location you entered
+
+url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=imperial&appid={apiKey}'
+
+response = requests.get(url) # makes a request to the url
+
+# print(f'\n{response.json()}') # prints json response
+
+high_weather = m.trunc(response.json()['main']['temp_max'])
+low_weather = m.trunc(response.json()['main']['temp_min'])
+current_weather = m.trunc(response.json()["main"]["temp"])
+
+print(
+    f'\nLow: {low_weather}°F\nHigh: {high_weather}°F\nCurrent: {current_weather}°F\n')
